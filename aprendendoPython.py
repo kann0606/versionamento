590ad960bd5a9c19d174aca78dd02ee1469690d5
Kauan <kauan_greffin@estudante.sesisenai.org.br>
2026-05-13 21:16:23 -0300

    Primeiro Projeto em Python, Minha Calculadora

---
+++ aprendendoPython.py
@@ -0,0 +1,52 @@
+# -----------------------------------------------------
+# CALCULADORA EM PYTHON
+# OPERAÇÕES BÁSICAS
+# -----------------------------------------------------
+
+
+def soma(a, b):
+    return a+b
+
+def subtracao(a, b):
+    return a-b
+
+def divisao(a, b):
+    if b == 0:
+        return "O divisor não pode ser igual a zero!"
+    else:
+        return a/b
+    
+def multiplicacao(a, b):
+    return a*b
+
+
+
+
+while True:
+    print("\n---------CALCULADORA----------")
+    print("Soma - 1")
+    print("Mutiplicação - 2")
+    print("Divisão - 3")
+    print("Subtração - 4")
+    print("Para Sair - 0")
+    
+    opcao = int(input("Escolha a operação desejada: "))
+    if opcao == 0:
+        break
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if opcao == 1:
+        resultado = soma(num1, num2)
+        
+    elif opcao == 2:
+        resultado = multiplicacao(num1, num2)
+
+    elif opcao == 3:
+        resultado = divisao(num1, num2)
+    
+    elif opcao == 4:
+        resultado = subtracao(num1, num2)
+    
+    print("---------RESULTADO--------")
+    print(f"Resultado = {resultado}")
